665e7e43280996a976a96fd4ef69ec7aff50f838
cse <cse@IP_129>
2017-03-07 09:34:10 +0900

    test2

---
+++ test_git/src/test_git/test.java
@@ -0,0 +1,12 @@
+package test_git;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("test");
+		System.out.print("test2");
+
+	}
+
+}
