2c5dac03729520b39b0af98ec73a682171e59ded
yuseon <altusdnjs@naver.com>
2017-03-07 10:10:27 +0900

    test

--- test_git/src/test_git/test.java
+++ test_git/src/test_git/test.java
@@ -8,7 +8,7 @@ public class test {
 		System.out.print("test2\n");
 		System.out.print("test3\n");
 		System.out.print("test4\n");
-		System.out.print("test6\n");
+		System.out.print("test7\n");
 	}
 
 }
