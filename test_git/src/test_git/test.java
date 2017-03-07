484883e3619a6e1028519c00049d6eb4c262ec81
kbbn2001 <cse@IP_129>
2017-03-07 09:51:54 +0900

    typing error fix

--- test_git/src/test_git/test.java
+++ test_git/src/test_git/test.java
@@ -4,11 +4,10 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("test/n");
-		System.out.print("test2/n");
-		System.out.print("test3/n");
-		System.out.print("test4/n");
-
+		System.out.print("test\n");
+		System.out.print("test2\n");
+		System.out.print("test3\n");
+		System.out.print("test4\n");
 	}
 
 }
